5845c30c5a61e7cd2b787fa4ac026e78f428ec97
Knut Rand <knutdrand@gmail.com>
2025-01-25 17:11:37 +0100

    docs: introduction to gridded model

--- chtorch/gridded_model/main.py
+++ chtorch/gridded_model/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import pytest
 from math import prod
 
+import scipy
+
 '''
 The main idea of the gridded model is to represeent the geospatial climate data in a grid,
 while the health data are represented in in aggregates over both polygons in space and different
@@ -25,8 +27,8 @@ T = 2
 M = 5
 N = 6
 PADDING = 1
-M_i = M - PADDING*2
-N_i = N - PADDING*2
+M_i = M - PADDING * 2
+N_i = N - PADDING * 2
 
 F = 2
 
@@ -64,7 +66,7 @@ def region_attribution() -> RegionAttribution:  # M, N
     """
     Matrices that represent how much of each pixel should be attributed to each region
     """
-    return {'Oslo': np.array([[1., 1., 1., 0.],
+    return {'oslo': np.array([[1., 1., 1., 0.],
                               [0.5, 1., 1., 0.5],
                               [0., 0., 1., 0.]])}
 
@@ -124,10 +126,55 @@ def pooling_function(gridded_data: FullGriddedArray,
     For now we assume that the vector dimension after the network is two, representing ca mean and variance.
     Thus we can just sum these over the network
     '''
-    return gridded_data * attribution_matrix[..., np.newaxis,:, :, np.newaxis]
+    return (gridded_data * attribution_matrix[..., np.newaxis, :, :, np.newaxis]).sum(axis=(-2, -3))
 
 
 def test_pooling_function(output_grid, region_attribution):
     region_parameters = pooling_function(output_grid, list(region_attribution.values())[0])
-    assert region_parameters.shape == (T, M_i, N_i, 2)
+    assert region_parameters.shape == (T, 2)
 
+
+# Now maybe we have some untransformed variables for the our distribution.
+# If the outcome is normally distributed, we dont need any transformations as sums of  normal varibles are normal with
+# sum of means and variances
+# Lognormal would also be ok, but count data might be different. We want the aggregated mean be the sum of the means, but
+# it is less simple how the variances are related. Thus what is remaining is both the transformation and the distribution.
+# Taking
+# Note that the means should probably be exped or softmaxed before summing
+
+def get_parameters(raw_params: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    return np.exp(raw_params[..., 0]), np.sqrt(np.exp(raw_params[..., 1]))
+
+
+def log_pdf(parameters, observed):
+    return scipy.stats.norm.logpdf(observed - parameters[0] / parameters[1])
+
+
+def test_log_pdf(mixed_data):
+    """
+    This is what the whole pipeline would look like from the initial
+    mixed data.
+    """
+    tensor = get_grid_tensor(mixed_data.gridded_data)
+    output_grid = my_network(tensor)
+    pooled = {region_name:
+                  pooling_function(output_grid, attribution_matrix=attribution_matrix)
+              for region_name, attribution_matrix in mixed_data.region_attribution.items()}
+    log_pdfs = {region_name:
+                    log_pdf(get_parameters(pooled_params), mixed_data.aggregated_data[region_name])
+                for region_name, pooled_params in pooled.items()}
+    assert len(log_pdfs) == 1
+    assert next(iter(log_pdfs.values())).shape == (T,)
+
+
+'''
+What's missing now is an additional temporal aggregation step, which would need some temporal attribution
+akin to the spatioal attribution.
+
+We also have to figure out how the eta's for each pixel should be pooled in order to
+as simply as possible represent the sum of different negative binomial variables.
+
+As for networks, the feature dimension should be denesly connected while the
+gemotric relation ship are more natural to get in terms convolutional connections.
+A simple RNN should suffice for the temporal dimension.
+'''
