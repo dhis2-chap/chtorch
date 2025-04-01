88913016f9649f37a592814763b83636facd6223
Knut Rand <knutdrand@gmail.com>
2025-04-01 11:46:03 +0200

    feat: hierarchical l2 Fixes #6

--- chtorch/lightning_module.py
+++ chtorch/lightning_module.py
@@ -33,17 +33,26 @@ class DeepARLightningModule(L.LightningModule):
 
     def configure_optimizers(self):
         decay = []
+        embed_decay = []
+        level_2_decay = []
         no_decay = []
         for name, param in self.named_parameters():
             if not param.requires_grad:
                 continue
             if name.endswith("bias") or "norm" in name.lower():
                 no_decay.append(param)
+            elif 'embed' in name:
+                if '.1.' in name:
+                    level_2_decay.append(param)
+                else:
+                    embed_decay.append(param)
             else:
                 decay.append(param)
 
         return optim.AdamW([
             {"params": decay, "weight_decay": self.weight_decay},
+            {"params": embed_decay, "weight_decay": self.weight_decay * 10},
+            {"params": level_2_decay, "weight_decay": self.weight_decay * 100},
             {"params": no_decay, "weight_decay": 0.0}
         ], lr=1e-3)
         #
