f308ed9bdfe9ba6279841ab95944f5d3c619bd9b
Anne Klier <anne.klier@iis.fraunhofer.de>
2025-08-22 08:39:35 +0200

    adding benchmark script with yacs configuration

--- chtorch/configuration.py
+++ chtorch/configuration.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 from chtorch.module import RNNConfiguration
+from yacs.config import CfgNode as CN
 
 
 class TensorifierConfig(BaseModel): # data preprocessing config
@@ -31,3 +32,38 @@ class ProblemConfiguration(BaseModel):
     validate: bool = False
     validation_splits: int = 5
     validation_index: int = 4
+
+
+
+_C = CN()
+
+_C.NAME = CN()
+_C.NAME.EXPERIMENT_NAME = "PBMOHPO"
+
+_C.PROBLEM = CN()
+_C.PROBLEM.PROBLEM_TYPE = "TuneDeepAR"
+_C.PROBLEM.DATAPATH = "data/vietnam_monthly.csv"
+
+_C.OPTIMIZER = CN()
+_C.OPTIMIZER.OPTIMIZER_TYPE = "BO"
+
+_C.DECISION_MAKER = CN()
+_C.DECISION_MAKER.DECISION_MAKER_TYPE = "DecisionMaker"
+_C.DECISION_MAKER.SEED = 123
+
+_C.BUDGET = CN()
+_C.BUDGET.EVAL_BUDGET = 50
+_C.BUDGET.DM_BUDGET = 50
+
+_C.BATCH_SIZE = CN()
+_C.BATCH_SIZE.EVAL_BATCH_SIZE = 1
+_C.BATCH_SIZE.DM_BATCH_SIZE = 1
+
+_C.REPL = CN()
+_C.REPL.SEEDREPL = 0
+
+def get_cfg_defaults():
+    """Get a yacs CfgNode object with default values for my_project."""
+    # Return a clone so that the defaults will not be altered
+    # This is for the "local variable" use pattern
+    return _C.clone()
---
+++ scripts/run_pbmohpo_bench.py
@@ -0,0 +1,240 @@
+import argparse
+import logging
+import os
+import tempfile
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import mlflow
+import pandas as pd
+from mlflow import log_artifact, log_metric, log_metrics, log_params
+
+from chtorch.preferential_optimization import TuneDeepAR
+from chtorch.configuration import get_cfg_defaults
+
+from pbmohpo.benchmark import Benchmark
+from pbmohpo.decision_makers.decision_maker import DecisionMaker
+from pbmohpo.optimizers.eubo import EUBO, qEUBO
+from pbmohpo.optimizers.random_search import RandomSearch
+from pbmohpo.optimizers.utility_bayesian_optimization import UtilityBayesianOptimization
+
+from pbmohpo.utils import visualize_archives
+
+
+def run_pbmohpo_bench(
+    config,
+    visualize: bool = False,
+    use_mlflow: bool = False,
+    save_archive: bool = False,
+):
+    """
+    Run a preferential bayesian hyperparameter optimization benchmark as
+    specified in the config file.
+
+    Parameters
+    ----------
+    config: str
+    path to config.yaml file. File should be in the following format.
+    --
+    PROBLEM:
+      PROBLEM_TYPE: "yahpo"
+      ID: "iaml_ranger"
+      INSTANCE: "1067"
+      OBJECTIVE_NAMES: ["auc", "nf"]
+      OBJECTIVE_SCALING_FACTORS: [1, 21]
+
+    FIXED_HPS:
+      TRAINSIZE: ("trainsize", 1)
+      REPLACE: ("replace", "TRUE")
+      RESPECT_UNORDERED_FACTORS: ("respect.unordered.factors", "ignore")
+      SPLITRULE: ("splitrule", "gini")
+    --
+    For options than can be set in config files, please see config.py.
+
+    visualize: bool
+    Specify whether to create plots (with default configuration) for
+    benchmark.
+
+    use_mlflow: bool
+    Should the experiment be tracked with mlflow.
+
+    save_archive: bool
+    Should the archive be saved to a file.
+    """
+
+    # set all relevant random seeds based on seedrepl
+    # this is necessary to ensure reproducibility
+    if config.REPL.SEEDREPL:
+        logging.info(f"Setting all random seeds to {config.REPL.SEEDREPL}")
+        import random
+
+        import numpy as np
+        import torch
+
+        random.seed(config.REPL.SEEDREPL)
+        np.random.seed(config.REPL.SEEDREPL)
+        torch.manual_seed(config.REPL.SEEDREPL)
+        torch.cuda.manual_seed(config.REPL.SEEDREPL)
+        torch.cuda.manual_seed_all(config.REPL.SEEDREPL)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    if use_mlflow:
+        mlflow.set_experiment(config.NAME.EXPERIMENT_NAME)
+        print(config)
+        print(config.values())
+        for c_item in config.values():
+            log_params(c_item)
+
+    if config.PROBLEM.PROBLEM_TYPE == "TuneDeepAR":
+        logging.info("Testing TuneDeepAR")
+        prob = TuneDeepAR(data_path = config.PROBLEM.DATAPATH)
+
+    if config.OPTIMIZER.OPTIMIZER_TYPE == "RS":
+        logging.info("Running Random Search")
+        opt = RandomSearch(prob.get_config_space())
+    elif config.OPTIMIZER.OPTIMIZER_TYPE == "BO":
+        logging.info("Running Bayesian Optimization on Utility Scores")
+        opt = UtilityBayesianOptimization(prob.get_config_space())
+    elif config.OPTIMIZER.OPTIMIZER_TYPE == "qEUBO":
+        logging.info("Running Bayesian Optimization on Pairwise Comparisons (qEUBO)")
+        opt = qEUBO(prob.get_config_space())
+    else:
+        logging.info("Running Bayesian Optimization on Pairwise Comparisons")
+        opt = EUBO(prob.get_config_space())
+
+    dm = DecisionMaker(
+        objective_names=prob.get_objective_names(), seed=config.DECISION_MAKER.SEED
+    )
+
+    logging.info("Decision Maker Preference Scores:")
+    logging.info(dm.preferences)
+
+    bench = Benchmark(
+        prob,
+        opt,
+        dm,
+        eval_budget=config.BUDGET.EVAL_BUDGET,
+        dm_budget=config.BUDGET.DM_BUDGET,
+        eval_batch_size=config.BATCH_SIZE.EVAL_BATCH_SIZE,
+        dm_batch_size=config.BATCH_SIZE.DM_BATCH_SIZE,
+    )
+    bench.run()
+
+    archive = bench.archive
+    best_eval = archive.evaluations[archive.incumbents[0]]
+
+    if use_mlflow:
+        log_metric("utility", best_eval.utility)
+        log_metrics(best_eval.objectives)
+
+    logging.info(f"Best Configuration found in iteration [{archive.incumbents[0]}]:")
+    logging.info(best_eval)
+
+    if visualize:
+        visualize_archives(archive_list=[archive])
+        if use_mlflow:
+            logging.info("Storing trace")
+            plt.savefig(f"{tempfile.gettempdir()}/trace.png")
+            log_artifact(f"{tempfile.gettempdir()}/trace.png")
+        else:
+            plt.show()
+
+    # FIXME: maybe also save archive to mlflow
+    if save_archive:
+        logging.info("Saving archive")
+        x, y = archive.to_numpy()
+        df = pd.DataFrame(y, columns=["utility"])
+        df["best"] = df["utility"].cummax()
+        df["prob"] = config.PROBLEM.PROBLEM_TYPE
+        df["opt"] = config.OPTIMIZER.OPTIMIZER_TYPE
+        df["seed"] = str(config.DECISION_MAKER.SEED)
+        df["seedrepl"] = str(config.REPL.SEEDREPL)
+        df["iter"] = range(1, len(df) + 1)
+        if len(df) is not config.BUDGET.EVAL_BUDGET:
+            logging.warning("Archive has not been fully populated")
+        path = "experiment_results"
+        subpath = df["prob"][0]
+        path = os.path.join(path, subpath)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        file = (
+            os.path.join(path, df["prob"][0] + "_" + df["opt"][0])
+            + "_"
+            + str(df["seed"][0])
+            + "_"
+            + str(df["seedrepl"][0])
+            + "_"
+            + datetime.now().strftime("%y%m%d%H%M%S")
+            + ".csv"
+        )
+        df.to_csv(file, index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Specify experiment to run")
+
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="Config file of experiment",
+        default="./experiment_configs/example_config.yaml",
+        dest="config",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--debug",
+        help="Print debugging statements",
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+        default=logging.WARNING,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Be verbose",
+        action="store_const",
+        dest="loglevel",
+        const=logging.INFO,
+    )
+
+    parser.add_argument(
+        "-p",
+        "--plot",
+        help="creates plots (default config) of conducted benchmark",
+        action="store_true",
+        dest="visualize",
+    )
+
+    parser.add_argument(
+        "-m",
+        "--mlflow",
+        help="Should experiment be tracked by mlflow",
+        action="store_true",
+        dest="use_mlflow",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--save_archive",
+        help="Should the archive be saved to a file",
+        action="store_true",
+        dest="save_archive",
+    )
+
+    args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel)
+
+    cfg = get_cfg_defaults()
+    cfg.merge_from_file(args.config)
+    cfg.freeze()
+    logging.debug(cfg)
+
+    run_pbmohpo_bench(
+        cfg,
+        visualize=args.visualize,
+        use_mlflow=args.use_mlflow,
+        save_archive=args.save_archive,
+    )
